7b0839d54fa1e2786986514c0ccfec628c4ecbc9
cream <837800910@qq.com>
2024-06-23 06:48:59 +0800

    引入mybatis-plus

--- ROW-backend/src/main/java/com/cream/row/RowBackendApp.java
+++ ROW-backend/src/main/java/com/cream/row/RowBackendApp.java
@@ -1,8 +1,10 @@
 package com.cream.row;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.cream.row.mapper")
 @SpringBootApplication
 public class RowBackendApp {
 
---
+++ ROW-backend/src/main/resources/sql/DbOfMysql8.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `row`.t_user
+(
+    id       BIGINT      NOT NULL,
+    username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    CONSTRAINT t_user_pk PRIMARY KEY (id),
+    CONSTRAINT t_user_unique UNIQUE KEY (username)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
