9b85e7605c19d85e229b73a1b05b3ba6fd73353b
cream <837800910@qq.com>
2024-06-23 02:17:47 +0800

    basic configuration

--- ROW-backend/src/main/java/com/cream/row/RowBackendApp.java
+++ ROW-backend/src/main/java/com/cream/row/RowBackendApp.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RowBackendApplication {
+public class RowBackendApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(RowBackendApplication.class, args);
+        SpringApplication.run(RowBackendApp.class, args);
     }
-
 }
--- ROW-backend/src/test/java/com/cream/row/RowBackendAppTests.java
+++ ROW-backend/src/test/java/com/cream/row/RowBackendAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RowBackendApplicationTests {
+class RowBackendAppTests {
 
     @Test
     void contextLoads() {
