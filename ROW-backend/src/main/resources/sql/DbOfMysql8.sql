1c589e05d6e756a986cef6c054df0bc2f6887ced
cream <837800910@qq.com>
2024-06-23 07:32:22 +0800

    fix: 修改数据库名称（命名误使用关键字）

--- ROW-backend/src/main/resources/sql/DbOfMysql8.sql
+++ ROW-backend/src/main/resources/sql/DbOfMysql8.sql
@@ -1,8 +1,11 @@
-CREATE TABLE IF NOT EXISTS `row`.t_user
+CREATE TABLE IF NOT EXISTS `d_row`.t_user
 (
     id       BIGINT      NOT NULL,
     username varchar(50) NOT NULL,
     password varchar(50) NOT NULL,
+    `deleted`      tinyint(1)  NOT NULL,
+    `create_time`  datetime    NOT NULL,
+    `update_time`  datetime    NOT NULL,
     CONSTRAINT t_user_pk PRIMARY KEY (id),
     CONSTRAINT t_user_unique UNIQUE KEY (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
