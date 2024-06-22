2120d2219139a3979c69c40bd7106b989d835dff
cream <837800910@qq.com>
2024-06-23 02:04:39 +0800

    init backend project

---
+++ ROW-backend/src/main/java/com/cream/row/RowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cream.row;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RowBackendApplication.class, args);
+    }
+
+}
---
+++ ROW-backend/src/test/java/com/cream/row/RowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cream.row;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
