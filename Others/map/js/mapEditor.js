956a4d56bf597e4b18259ef95a9127790820f140
cream <837800910@qq.com>
2024-06-23 21:05:09 +0800

    绘制地图网格

--- Others/map/js/mapEditor.js
+++ Others/map/js/mapEditor.js
@@ -1,4 +1,4 @@
-import PointMeta from "./PointMeta.js";
+import drawMapGrid from "./sub/drawMapGrid.js";
 
 const width = parseInt(prompt("输入地图宽"));
 const height = parseInt(prompt("输入地图高"));
@@ -8,38 +8,5 @@ const ctx = canvas.getContext("2d");
 canvas.width = width;
 canvas.height = height;
 
+drawMapGrid(ctx, width, height)
 
-let pointArray = []
-for (let y = 0; y < height; y++) {
-    let row = [];
-    for (let x = 0; x < width; x++) {
-        row.push(new PointMeta(x, y))
-    }
-    pointArray.push(row)
-}
-
-const gridLineColor = "lightgray"
-const lineWidth = 1;
-const gridSize = 50;
-
-function drawGridLines() {
-    ctx.lineWidth = lineWidth
-    ctx.strokeStyle = gridLineColor
-    // 绘制垂直线
-    for (let x = 0; x < width; x += gridSize) {
-        ctx.beginPath()
-        ctx.moveTo(x, 0)
-        ctx.lineTo(x, height)
-        ctx.stroke()
-    }
-
-    // 绘制水平线
-    for (let y = 0; y < height; y += gridSize) {
-        ctx.beginPath();
-        ctx.moveTo(0, y)
-        ctx.lineTo(width, y)
-        ctx.stroke()
-    }
-}
-
-drawGridLines();
---
+++ Others/map/js/sub/drawMapGrid.js
@@ -0,0 +1,38 @@
+import PointMeta from "../PointMeta.js";
+
+export default function drawMapGrid(ctxOf2D, width, height) {
+    // todo pointArray用来初始化地图格子对象，后续应该有用
+    let pointArray = []
+    for (let y = 0; y < height; y++) {
+        let row = [];
+        for (let x = 0; x < width; x++) {
+            row.push(new PointMeta(x, y))
+        }
+        pointArray.push(row)
+    }
+    function drawGridLines() {
+        const gridLineColor = "lightgray"
+        const lineWidth = 1;
+        const gridSize = 50;
+
+        ctxOf2D.lineWidth = lineWidth
+        ctxOf2D.strokeStyle = gridLineColor
+        // 绘制垂直线
+        for (let x = 0; x < width; x += gridSize) {
+            ctxOf2D.beginPath()
+            ctxOf2D.moveTo(x, 0)
+            ctxOf2D.lineTo(x, height)
+            ctxOf2D.stroke()
+        }
+
+        // 绘制水平线
+        for (let y = 0; y < height; y += gridSize) {
+            ctxOf2D.beginPath();
+            ctxOf2D.moveTo(0, y)
+            ctxOf2D.lineTo(width, y)
+            ctxOf2D.stroke()
+        }
+    }
+
+    drawGridLines();
+}
